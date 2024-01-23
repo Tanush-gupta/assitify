428372beeb7ad722de8d653f0be5683051f7c9dc
Tanush Gupta <72255106+Tanush-gupta@users.noreply.github.com>
2024-01-23 18:46:04 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,10 @@
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import AppNavigation from './src/navigation';
+export default function App() {
+  return (
+    <AppNavigation />
+  )
+}
+
+const styles = StyleSheet.create({});
